b65448bd91fd1778af811a8fb9204ff9af6824ed
Harsh-Mevada <harshm5220@gmail.com>
2026-04-26 15:56:14 +0530

    Modify database URI and secret key
    Updated database URI and secret key in config.py.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:harsh1975@localhost/evidence_db"
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres/evidence_db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "hm2408hm"
+SECRET_KEY = "password"
 UPLOAD_FOLDER = "uploads"
